754c48f9f74fbf6c9298a49b000248dfc65473a6
wjl <934366858@qq.com>
2018-12-25 22:22:55 +0800

    wirte ___comments____

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from blog import views
 urlpatterns = [
     path('', views.index.as_view()),
     path('category/<int:category>',views.CategoryList.as_view(), name ='category'),
+    path('search/',views.Search.as_view(), name='search'),
+    path('detail/', views.AritcleDetail.as_view(), name= 'detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from blog.models import *
-from django.views.generic import ListView
-
+from django.views.generic import ListView,DetailView
+from django.db.models import Q
+from .forms import CommentForm
 # Create your views here.
 
 class index(ListView):
@@ -27,3 +28,52 @@ class CategoryList(ListView):
         context['category'] = category.name
         print('----------------------')
         return context
+
+class Search(ListView):
+    model = Article
+    template_name = 'search.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        key = self.request.GET['key']
+        if key:
+            return Article.objects.filter(Q(title__icontains=key)|Q(content__icontains=key)).order_by('-id')
+        else:
+            return None
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['key'] = self.request.GET['key']
+        return context
+
+def comment_sort(self,comments):
+    self.commet_list = []
+    self.top_level = []
+    self.sub_level = {}
+    for comment in comments:
+        if comment.reply == None:
+            self.top_level.append(comment)
+        else:
+            self.sub_level.setdefault(comment.reply.id,[]).append(comment)
+
+def format_show(self,top_comment):
+    self.comment_lsit.append(top_comment)
+    try:
+        self.kids = self.sub_level[top_comment.id]
+    except KeyError:
+        pass
+    else:
+        for kid in self.kids:
+            self.format_show(kid)
+class AritcleDetail(DetailView):
+    model = Article
+    template_name = 'detail.html'
+
+    def get_context_data(self, **kwargs):
+        comment_form = CommentForm
+        content = super().get_context_data(**kwargs)
+        comments = Comment.objects.filter(article=self.kwargs['pk'])
+        content['commetn_list'] = comment_sort(comments)
+        content['comment_form'] = comment_form
+        return content
+
+
