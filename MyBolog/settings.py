0a662da8720816e672658fe925da31cfaa22617e
wjl <934366858@qq.com>
2018-12-25 16:10:20 +0800

    ss

--- MyBolog/settings.py
+++ MyBolog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+print(STATICFILES_DIRS)
 
 
 DATABASES = {
