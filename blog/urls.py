7dd403182df08d6491c3e85a63af7ee47e55218a
wjl <934366858@qq.com>
2018-12-25 18:14:18 +0800

    aa

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from blog import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index.as_view()),
+    path('category/<int:category>',views.CategoryList.as_view(), name ='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from blog.models import *
+from django.views.generic import ListView
 
 # Create your views here.
 
-def index(request):
-    return  render(request,'base.html')
+class index(ListView):
+    model = Article
+    template_name = 'index.html'
+    queryset_name = Article.objects.all().order_by('-id')
+    paginate_by = 5
+
+
+class CategoryList(ListView):
+    model = Article
+    template_name = 'category.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Article.objects.filter(category=self.kwargs['category']).order_by('-id')
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category = Category.objects.get(id=self.kwargs['category'])
+        print(category.name)
+        context['category'] = category.name
+        print('----------------------')
+        return context
